fd9142016f4b68a67a4e1b32e505184b9296a5ca
MartinSVT <106102951+MartinSVT@users.noreply.github.com>
2025-03-18 13:59:17 +0200

    Navigation Page with Styles
    Adding Navigation Functionality (react router) with styles using css modules

--- Front_End_React/dbe-fe/src/App.jsx
+++ Front_End_React/dbe-fe/src/App.jsx
@@ -1,17 +1,17 @@
 import { Route, Routes } from 'react-router'
-import './App.css'
+import globalStyles from './App.module.css'
 import Header from './components/HeaderComponent/HeaderComp'
 import Home from './components/HomeComponent/HomeComp'
 
 function App() {
 
   return (
-    <>
+    <div className={globalStyles.main_body}>
       <Header/>
       <Routes>
         <Route path='/home' element={<Home />}></Route>
       </Routes>
-    </>
+    </div>
   )
 }
 
--- Front_End_React/dbe-fe/src/components/HeaderComponent/HeaderComp.jsx
+++ Front_End_React/dbe-fe/src/components/HeaderComponent/HeaderComp.jsx
@@ -1,50 +1,70 @@
-import {Link} from 'react-router';
+import {Link, NavLink} from 'react-router';
+import localStyles from './HeaderCompCSS.module.css'
+
 
 export default function Header() {
-
+    let isLoggedIn = true;
+    let isStaff = true;
     return (
         <>
-            <header className="base_header">
-                <nav className="base_nav">
-                    <div className="general_nav">
-                        <Link to="/home">Home</Link>
-                        <Link to="/contacts">Contacts</Link>
-                        <Link to="/about">About Us</Link>
+            <header className={localStyles.header_class}>
+                <nav className={localStyles.base_nav}>
+                    <div className={localStyles.general_nav}>
+                        <NavLink to="/home">Home</NavLink>
+                        <NavLink to="/contacts">Contacts</NavLink>
+                        <NavLink to="/about">About Us</NavLink>
                     </div>
-                    <div className="logo">
+                    <div className={localStyles.logo}>
                         <Link to="/home">
-                            <img className="logo_img" src='/DBE_logo.jpg'/>
+                            <img className={localStyles.logo_img }src='/DBE_logo.jpg'/>
                         </Link>
-
-                        {/* <p *ngIf="isLoggedIn"> Hello {{CurrentUserData.username}}</p> */}
-                        <p> Hello Guest</p>
+                            {isLoggedIn ? (<p>Hello User</p>) : (<p>Hello Guest</p>)}
                     </div>
-                    <div className="type_nav">
-                            {/* <ng-container *ngIf="isLoggedIn">
-                                <ng-container *ngIf="CurrentUserData.is_staff==true"> */}
-                                    <Link to="/operations">Operations</Link>
-                                    <Link to="/addArticle">Add Article</Link> 
-                                {/* </ng-container>  
-                            </ng-container>  */}
-                            {/* <ng-container *ngIf="isLoggedIn">
-                                <ng-container *ngIf="!CurrentUserData.is_staff==true"> */}
-                                    <Link to="/orders">Orders</Link>
-                                {/* </ng-container>  
-                            </ng-container>    */}
-                            {/* <ng-container *ngIf="isLoggedIn">
-                                <ng-container *ngIf="CurrentUserData.is_staff==true"> */}
-                                    <Link to="/staffOrders">Orders</Link>
-                                {/* </ng-container>  
-                            </ng-container>  */}
-                            <Link to="/viewProfile">Profile</Link>
-                            <Link to="/logout">Logout</Link>
-                            <Link to="/register">Register</Link>
-                            <Link to="/login">Login</Link>
+                    <div className={localStyles.type_nav}>
+                        {(() => {
+                            if (isLoggedIn) {
+                                if (isStaff) {
+                                    return (
+                                        <>
+                                        <NavLink to="/operations">Operations</NavLink>
+                                        <NavLink to="/addArticle">Add Article</NavLink> 
+                                        <NavLink to="/staffOrders">Orders</NavLink>
+                                        <NavLink to="/viewProfile">Profile</NavLink>
+                                        <NavLink to="/logout">Logout</NavLink>
+                                        <NavLink id={localStyles.logo_link} to="/viewProfile">
+                                            <div className={localStyles.prof_logo}>
+                                                <img src="/def_prof_pic.jpg"/>
+                                            </div>
+                                        </NavLink>
+                               
+                                        </>
+                                    )
+                                } else if (!isStaff) {
+                                    return (
+                                        <>
+                                        <NavLink to="/orders">Orders</NavLink>
+                                        <NavLink to="/viewProfile">Profile</NavLink>
+                                        <NavLink to="/logout">Logout</NavLink>
+                                        <NavLink id={localStyles.logo_link} to="/viewProfile">
+                                            <div className={localStyles.prof_logo}>
+                                                    <img src="/def_prof_pic.jpg"/>
+                                            </div>
+                                        </NavLink>
+              
+                                        </>
+                                    )
+                                }
+                            } else {
+                                return(
+                                    <>
+                                        <NavLink to="/login">Login</NavLink>
+                                        <NavLink to="/register">Register</NavLink>
+                                    </>
+                                )
+                            }
+                        })()}
                     </div>
                 </nav>
-                <div className="prof_logo">
-                    <img src="/def_prof_pic.jpg" alt=""/>
-                </div>
             </header>
       </>
     )
